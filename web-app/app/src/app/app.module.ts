1c23508e161093c72bfa3e7a0aaf3018c2978dce
deojonathan <deojonathanguillermo22@gmail.com>
2020-10-28 00:13:22 +0800

    Base angular structure with login component and material module

---
+++ web-app/app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ web-app/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Convenience script for running docker container of this service.
+FWD_TO_PORT="8080"
+DOCKER_VOLUMES="${PWD}/app/src:/usr/src/app/src"
+DOCKER_IMG_TAG="${PWD##*/}:dev"
+
+docker build -t $DOCKER_IMG_TAG -f Dockerfile.dev . && \
+docker run -p $FWD_TO_PORT:80 -v $DOCKER_VOLUMES $DOCKER_IMG_TAG\
+              npm run start -- --host --poll 500 --port 80
