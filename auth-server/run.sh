f24d4b2f2d9cc4d9513896cc6070837980f7eb7a
deojonathan <deojonathanguillermo22@gmail.com>
2020-11-04 12:59:02 +0800

    Update (minor) run script for auth-server - moved env vars above script for easier visibility

--- auth-server/run.sh
+++ auth-server/run.sh
@@ -3,12 +3,13 @@
 GUNICORN_APP="gunicorn"
 GUNICORN_ARGS="--bind 0.0.0.0:80 -w 4 --reload app:flask_app"
 FWD_TO_PORT="8081"
+ENV_VARS="--env SPOTIFY_CLIENT_ID --env SPOTIFY_CLIENT_SECRET"
 
 DOCKER_VOLUMES="${PWD}/app:/usr/src/app"
 DOCKER_IMG_TAG="${PWD##*/}:dev"
 
 pip freeze | grep -v "pkg-resources" > requirements.txt
 docker build -t $DOCKER_IMG_TAG . && \
-docker run --env SPOTIFY_CLIENT_ID --env SPOTIFY_CLIENT_SECRET \
+docker run $ENV_VARS \
            -p $FWD_TO_PORT:80 -v $DOCKER_VOLUMES $DOCKER_IMG_TAG \
            $GUNICORN_APP $GUNICORN_ARGS
