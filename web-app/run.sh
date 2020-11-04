0592a6f99055332a60e312a8fc6ae8afeda713c2
deojonathan <deojonathanguillermo22@gmail.com>
2020-11-04 12:56:56 +0800

    Add base layout for login page, update docker-compose (fix Invalid host header error, add env variables)

--- web-app/run.sh
+++ web-app/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Convenience script for running docker container of this service.
 FWD_TO_PORT="8080"
-DOCKER_VOLUMES="${PWD}/app/src:/usr/src/app/src"
+DOCKER_VOLUMES="${PWD}/app/src:/usr/src/app/src:ro"
 DOCKER_IMG_TAG="${PWD##*/}:dev"
 
 docker build -t $DOCKER_IMG_TAG -f Dockerfile.dev . && \
